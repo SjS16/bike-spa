954975f3d330d51a248c0d9313e352c75d66b3c2
Steph Simpson <stephsimpson911@gmail.com>
2020-07-12 18:47:30 -0700

    second page and routing to make sure that part is right

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
+  { path: 'about-us', component: AboutPageComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: HomePageComponent }
 ];
