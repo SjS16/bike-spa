1f6b8749642cb560e660fdd72b20d01aefad4a8f
Steph Simpson <stephsimpson911@gmail.com>
2020-07-12 18:44:01 -0700

    basic routing and homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: HomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
